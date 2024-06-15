689bac270c601af825c65daecd33fbdd522c0452
daniel-dunsin <adejaredaniel12@gmail.com>
2024-06-15 11:33:47 +0100

    feat: doctor schema

---
+++ src/api/doctor/enums/index.ts
@@ -0,0 +1,9 @@
+export enum DoctorSpeciality {
+   CARDIOLOGIST = 'cardiologist',
+   DENTIST = 'dentist',
+   NEUROLOGIST = 'neurologist',
+   ORTHOPEDIC = 'orthopedic',
+   UROLOGIST = 'urologist',
+   OPTOMETRIST = 'optometrist',
+   PYSCHOTHERAPIST = 'pyschotherapist',
+}
--- src/api/doctor/schema/doctor.schema.ts
+++ src/api/doctor/schema/doctor.schema.ts
@@ -2,6 +2,7 @@ import { Prop, Schema } from '@nestjs/mongoose';
 import { Types } from 'mongoose';
 import { User, UserDocument } from 'src/api/user/schema/user.schema';
 import { schemaOptions } from 'src/shared/constants/db.const';
+import { DoctorSpeciality } from '../enums';
 
 @Schema(schemaOptions)
 export class Doctor {
@@ -10,4 +11,16 @@ export class Doctor {
       ref: User.name,
    })
    user: UserDocument | string;
+
+   @Prop()
+   experienceYears: number;
+
+   @Prop({
+      type: String,
+      enum: Object.values(DoctorSpeciality),
+   })
+   speciality: DoctorSpeciality;
+
+   @Prop()
+   qualifications: string[];
 }
