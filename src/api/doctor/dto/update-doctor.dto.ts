94adb0b8e8ebd61a77cbd07e8f3d0436d2f8b129
daniel-dunsin <adejaredaniel12@gmail.com>
2024-06-15 16:10:21 +0100

    feat: update doctor available days

--- src/api/doctor/dto/update-doctor.dto.ts
+++ src/api/doctor/dto/update-doctor.dto.ts
@@ -1,22 +1,33 @@
 import { Gender } from 'src/api/user/enums';
-import { IsString, IsEnum, IsNumber } from 'src/shared/decorators';
-import { DoctorSpeciality } from '../enums';
+import { IsString, IsEnum, IsNumber, IsDate } from 'src/shared/decorators';
+import { Days, DoctorSpeciality } from '../enums';
 import { ApiProperty } from '@nestjs/swagger';
-import { ValidateNested } from 'class-validator';
+import { IsArray, IsOptional, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class SocialLinks {
-   @IsString(false)
+   @IsString(true)
    facebook?: string;
 
-   @IsString(false)
+   @IsString(true)
    whatsapp?: string;
 
-   @IsString(false)
-   twitter: string;
+   @IsString(true)
+   twitter?: string;
 
-   @IsString(false)
+   @IsString(true)
    linkedin?: string;
+}
+
+export class AvailableDay {
+   @IsEnum(Days, false)
+   day: Days;
+
+   @IsDate(false)
+   startTime: Date;
+
+   @IsDate(false)
+   endTime: Date;
 }
 
 export class UpdateDoctorDto {
@@ -41,5 +52,13 @@ export class UpdateDoctorDto {
    @ApiProperty({ type: SocialLinks })
    @ValidateNested()
    @Type(() => SocialLinks)
+   @IsOptional()
    socials: SocialLinks;
+
+   @ApiProperty({ type: [AvailableDay] })
+   @IsArray()
+   @ValidateNested({ each: true })
+   @Type(() => AvailableDay)
+   @IsOptional()
+   availableDays: AvailableDay[];
 }
