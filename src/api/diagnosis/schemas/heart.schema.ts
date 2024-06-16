b6344db220f69d45ab66744a5786aaedd8bd9730
daniel-dunsin <adejaredaniel12@gmail.com>
2024-06-16 02:29:45 +0100

    feat: heart metric schema

--- src/api/diagnosis/schemas/heart.schema.ts
+++ src/api/diagnosis/schemas/heart.schema.ts
@@ -10,6 +10,36 @@ export class HeartMetrics {
       required: true,
    })
    patient: PatientDocument | string;
+
+   @Prop()
+   heartRate: number; // in bpm
+
+   @Prop()
+   bloodPressureSystolic: number; // in mmHg
+
+   @Prop()
+   bloodPressureDiastolic: number; // in mmHg
+
+   @Prop()
+   bloodOxygenLevel: number; // in %
+
+   @Prop()
+   cholesterolTotal: number; // in mg/dL
+
+   @Prop()
+   cholesterolLDL: number; // in mg/dL
+
+   @Prop()
+   cholesterolHDL: number; // in mg/dL
+
+   @Prop()
+   ejectionFraction: number; // in %
+
+   @Prop()
+   cardiacOutput: number; // in L/min
+
+   @Prop()
+   bloodGlucoseLevel: number; // in mg/dL
 }
 
 export type HeartMetricsDocument = HydratedDocument<HeartMetrics>;
