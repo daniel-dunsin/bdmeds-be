1d441a2e68d003ccffaccb53f0169df021e12863
daniel-dunsin <adejaredaniel12@gmail.com>
2024-06-16 01:20:24 +0100

    feat: get fav doctors

--- src/api/patient/patient.controller.ts
+++ src/api/patient/patient.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Param, Put } from '@nestjs/common';
+import {
+   Body,
+   Controller,
+   Delete,
+   Get,
+   Param,
+   Post,
+   Put,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { PatientProvider } from './patient.provider';
-import { Auth } from 'src/shared/decorators/auth.decorators';
+import { Auth, Roles } from 'src/shared/decorators/auth.decorators';
 import { MongoIdPipe } from 'src/core/pipes';
 import { UpdatePatientDto } from './dto/update-patient.dto';
+import { RoleNames } from '../user/enums';
 
 @Controller('patient')
 @ApiTags('patient')
@@ -12,6 +21,7 @@ export class PatientController {
    constructor(private readonly patientProvider: PatientProvider) {}
 
    @Get('/user')
+   @Roles([RoleNames.PATIENT])
    async getUserPatient(@Auth('_id') userId: string) {
       const data = await this.patientProvider.getUserPatient(userId);
 
@@ -26,6 +36,7 @@ export class PatientController {
    }
 
    @Put()
+   @Roles([RoleNames.PATIENT])
    async updatePatient(
       @Auth('_id') userId: string,
       @Body() updatePatientDto: UpdatePatientDto,
@@ -34,6 +45,43 @@ export class PatientController {
          updatePatientDto,
          userId,
       );
+
+      return data;
+   }
+
+   @Post('/favourite/doctor/:doctorId')
+   @Roles([RoleNames.PATIENT])
+   async addFavDoc(
+      @Auth('_id') userId: string,
+      @Param('doctorId') doctorId: string,
+   ) {
+      const data = await this.patientProvider.addFavDoc(doctorId, userId);
+
+      return data;
+   }
+
+   @Delete('/favourite/doctor/:doctorId')
+   @Roles([RoleNames.PATIENT])
+   async removeFavDoc(
+      @Auth('_id') userId: string,
+      @Param('doctorId') doctorId: string,
+   ) {
+      const data = await this.patientProvider.removeFavDoc(doctorId, userId);
+
+      return data;
+   }
+
+   @Get('/user/favourite/doctor')
+   @Roles([RoleNames.PATIENT])
+   async getUserPatientFavDocs(@Auth('_id') userId: string) {
+      const data = await this.patientProvider.getUserFavDoctors(userId);
+
+      return data;
+   }
+
+   @Get('/:patientId/favourite/doctor')
+   async getPatientFavDocs(@Param('patientId') patientId: string) {
+      const data = await this.patientProvider.getFavDoctors(patientId);
 
       return data;
    }
