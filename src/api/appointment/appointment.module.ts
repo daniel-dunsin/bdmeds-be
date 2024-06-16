b0308292272299474a2fb88da743cbcd01c51639
daniel-dunsin <adejaredaniel12@gmail.com>
2024-06-16 03:17:16 +0100

    feat: appointment providers

--- src/api/appointment/appointment.module.ts
+++ src/api/appointment/appointment.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppointmentController } from './appointment.controller';
 import { AppointmentService } from './appointment.service';
 import { AppointmentProvider } from './appointment.provider';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Appointment, AppointmentSchema } from './schemas/appointment.schema';
 
 @Module({
+   imports: [
+      MongooseModule.forFeatureAsync([
+         {
+            name: Appointment.name,
+            useFactory() {
+               const schema = AppointmentSchema;
+               return schema;
+            },
+         },
+      ]),
+   ],
    controllers: [AppointmentController],
    providers: [AppointmentService, AppointmentProvider],
 })
--- src/api/appointment/appointment.service.ts
+++ src/api/appointment/appointment.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Appointment, AppointmentDocument } from './schemas/appointment.schema';
+import { FilterQuery, Model, QueryOptions, UpdateQuery } from 'mongoose';
 
 @Injectable()
-export class AppointmentService {}
+export class AppointmentService {
+   constructor(
+      @InjectModel(Appointment.name)
+      private readonly _appointmentModel: Model<AppointmentDocument>,
+   ) {}
+
+   async createAppointment<T>(createAppointmentDto: T) {
+      const appointment =
+         await this._appointmentModel.create(createAppointmentDto);
+
+      return appointment;
+   }
+
+   async updateAppointment(
+      filter: FilterQuery<AppointmentDocument>,
+      update: UpdateQuery<AppointmentDocument>,
+      options?: QueryOptions<AppointmentDocument>,
+   ) {
+      const appointment = await this._appointmentModel.findOneAndUpdate(
+         filter,
+         update,
+         { new: true, runValidators: true, ...options },
+      );
+
+      return appointment;
+   }
+
+   async getAppointments(filter: FilterQuery<AppointmentDocument>) {
+      const appointments = await this._appointmentModel.find(filter);
+
+      return appointments;
+   }
+
+   async getAppointment(filter: FilterQuery<AppointmentDocument>) {
+      const appointment = await this._appointmentModel.findOne(filter);
+
+      return appointment;
+   }
+
+   async deleteAppointment(
+      filter: FilterQuery<AppointmentDocument>,
+      options?: QueryOptions<AppointmentDocument>,
+   ) {
+      const appointment = await this._appointmentModel.findOneAndDelete(
+         filter,
+         options,
+      );
+
+      return appointment;
+   }
+}
