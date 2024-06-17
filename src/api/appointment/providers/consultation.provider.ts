2c0de249c5d37d77fdb1f60b5b34108a211cb2d8
daniel-dunsin <adejaredaniel12@gmail.com>
2024-06-17 13:58:09 +0100

    fix: initialize consultation provider

--- src/api/appointment/providers/consultation.provider.ts
+++ src/api/appointment/providers/consultation.provider.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { ConsultationService } from '../services/consulation.service';
 import { AppointmentService } from '../services/appointment.service';
+import { MailService } from 'src/shared/mail/mail.service';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+/**
+ * create session
+ * check appointment
+ * create record
+ * create consultation
+ * send mail to patient about the report
+ */
 
 @Injectable()
 export class ConsultationProvider {
    constructor(
+      @InjectConnection() private readonly connection: Connection,
       private readonly consultationService: ConsultationService,
       private readonly appointmentService: AppointmentService,
+      private readonly mailService: MailService,
    ) {}
 }
