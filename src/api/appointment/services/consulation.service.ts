5eee1cda93be3df129e35ef82d32e96fe1e2b6ab
daniel-dunsin <adejaredaniel12@gmail.com>
2024-06-17 13:03:55 +0100

    feat: consultation service

--- src/api/appointment/services/consulation.service.ts
+++ src/api/appointment/services/consulation.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Consultation, ConsultationDocument } from '../schemas/consultation.schema';
+import { ClientSession, FilterQuery, Model, QueryOptions, UpdateQuery } from 'mongoose';
 
 @Injectable()
-export class ConsultationService {}
+export class ConsultationService {
+   constructor(
+      @InjectModel(Consultation.name) private readonly _consultationModel: Model<ConsultationDocument>,
+   ) {}
+
+   async createConsultation<T>(createConsultaionDto: T, session?: ClientSession) {
+      const consultation = new this._consultationModel(createConsultaionDto);
+
+      return await consultation.save({ session });
+   }
+
+   async getConsultation(filter: FilterQuery<ConsultationDocument>): Promise<ConsultationDocument> {
+      const consulation = await this._consultationModel.findOne(filter);
+
+      return consulation;
+   }
+
+   async getConsultations(filter: FilterQuery<ConsultationDocument>): Promise<ConsultationDocument[]> {
+      const consulation = await this._consultationModel.find(filter);
+
+      return consulation;
+   }
+
+   async updateConsultation(
+      filter: FilterQuery<ConsultationDocument>,
+      update: UpdateQuery<ConsultationDocument>,
+      options: QueryOptions<ConsultationDocument>,
+   ): Promise<ConsultationDocument> {
+      const consulation = await this._consultationModel.findOneAndUpdate(filter);
+
+      return consulation;
+   }
+
+   async deleteConsultation(
+      filter: FilterQuery<ConsultationDocument>,
+      options: QueryOptions<ConsultationDocument>,
+   ) {
+      const consulation = await this._consultationModel.findOneAndDelete(filter);
+
+      return consulation;
+   }
+}
