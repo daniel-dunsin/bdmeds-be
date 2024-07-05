edf9f11860bf8db0e18cf1674ba6cd932b5a8419
daniel-dunsin <adejaredaniel12@gmail.com>
2024-07-05 12:05:17 +0100

    feat: verify transaction

--- src/api/payment/payment.controller.ts
+++ src/api/payment/payment.controller.ts
@@ -1,18 +1,31 @@
-import { Controller, Post, Req, Res } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { WebhookResponse } from './interfaces';
 import { Request, Response } from 'express';
 import { WebhookService } from './services/webhooks.service';
+import { PaymentProvider } from './providers/payment.provider';
+import { IsPublic } from 'src/shared/decorators/auth.decorators';
 
 @Controller('payment')
 @ApiTags('payment')
 export class PaymentController {
-   constructor(private readonly webhookService: WebhookService) {}
+   constructor(
+      private readonly webhookService: WebhookService,
+      private readonly paymentProvider: PaymentProvider,
+   ) {}
 
    @Post('/webhook')
    async webhook(@Req() req: Request<object, object, WebhookResponse>, @Res() res: Response) {
       await this.webhookService.processWebhook(req);
 
       res.status(200).json({ message: 'webhook processed' });
+   }
+
+   @IsPublic()
+   @ApiBody({ schema: { type: 'object', properties: { reference: { type: 'string' } } } })
+   async verifyTransaction(@Body('reference') reference: string) {
+      const data = await this.paymentProvider.verifyTransaction(reference);
+
+      return data;
    }
 }
--- src/api/payment/providers/payment.provider.ts
+++ src/api/payment/providers/payment.provider.ts
@@ -1,6 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable, NotFoundException } from '@nestjs/common';
+import { PaymentService } from '../services/payment.service';
+import { ApiBody } from '@nestjs/swagger';
 
 @Injectable()
 export class PaymentProvider {
-   constructor() {}
+   constructor(private readonly paymentService: PaymentService) {}
+
+   async verifyTransaction(reference: string) {
+      const transaction = await this.paymentService.getPaymentAttempt({ reference });
+
+      if (!transaction) throw new NotFoundException('transaction not found');
+
+      return {
+         message: 'trasnaction fetched',
+         success: true,
+         data: transaction,
+      };
+   }
 }
